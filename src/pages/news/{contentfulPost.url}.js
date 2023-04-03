bcb71ec3bed0f4e4cdb58a7596d86d3c05618e3a
Dan Ramos <dramos287@gmail.com>
2023-04-03 16:35:18 -0400

    comment out stuff not working

--- src/pages/news/{contentfulPost.url}.js
+++ src/pages/news/{contentfulPost.url}.js
@@ -4,40 +4,41 @@ import PostTemplate from "../../templates/post-template"
 import Seo from "../../components/SEO"
 import Layout from "../../components/Layout"
 
-const Post = ({ data: { contentfulPost } }) => {
+const Post = () => {
   return (
     <>
-      <Seo title={contentfulPost.title} />
+      {/* <Seo title={contentfulPost.title} /> */}
       <Layout>
-        <PostTemplate {...contentfulPost} />
+        post should go here
+        {/* <PostTemplate {...contentfulPost} /> */}
       </Layout>
     </>
   )
 }
 
-export const data = graphql`
-  query postQuery($id: String) {
-    contentfulPost(id: { eq: $id }) {
-      title
-      introduction
-      content {
-        raw
-        references {
-          ... on ContentfulAsset {
-            contentful_id
-            gatsbyImageData(
-              width: 800
-              placeholder: BLURRED
-              formats: [AUTO, WEBP, AVIF]
-            )
-            description
-            __typename
-          }
-        }
-      }
-      createdAt(formatString: "DD MMMM, YYYY")
-    }
-  }
-`
+// export const data = graphql`
+//   query postQuery($id: String) {
+//     contentfulPost(id: { eq: $id }) {
+//       title
+//       introduction
+//       content {
+//         raw
+//         references {
+//           ... on ContentfulAsset {
+//             contentful_id
+//             gatsbyImageData(
+//               width: 800
+//               placeholder: BLURRED
+//               formats: [AUTO, WEBP, AVIF]
+//             )
+//             description
+//             __typename
+//           }
+//         }
+//       }
+//       createdAt(formatString: "DD MMMM, YYYY")
+//     }
+//   }
+// `
 
 export default Post
